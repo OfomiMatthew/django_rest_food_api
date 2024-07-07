60e231768db396edf91bd5a6f73e7eb50eafc4b7
MatthewOfomi <ofomimatthew7@gmail.com>
2024-07-07 22:41:35 +0100

    searching functionality implemented

--- foodApi/views.py
+++ foodApi/views.py
@@ -2,15 +2,23 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework import generics,status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,renderer_classes
 from .models import FoodItem, Category
 from .serializers import FoodItemSerializer, CategorySerializer
+from rest_framework.renderers import TemplateHTMLRenderer
 
 
 @api_view(['GET','POST'])
 def food_item(request):
   if request.method == 'GET':
     items = FoodItem.objects.select_related('category').all()
+    category_name = request.query_params.get('category') #filtering operation
+    to_price = request.query_params.get('to_price') #filtering operation
+    if category_name:
+      items = items.filter(category__title=category_name)
+    if to_price:
+      items = items.filter(price__lte=to_price)
+      
     serialized_item = FoodItemSerializer(items,many=True,context={'request':request})
     return Response(serialized_item.data) 
   if request.method =='POST':
